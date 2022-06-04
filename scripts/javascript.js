32f249b145b955dbebe2a5297afd9f8394e21dbf
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-04 00:59:44 -0400

    Get rock button working

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,18 +1,16 @@
-// Rock Paper Scissors (RPS)
 
-// Array of possible RPS moves
-let moveSet = ["ROCK", "PAPER", "SCISSORS"];
-
-// Simulate computer choosing random play
 function getComputerMove(){
+  // Array of possible RPS moves
+  let moveSet = ["ROCK", "PAPER", "SCISSORS"];
+  // Simulate computer choosing random play
   let randomNum = Math.floor(Math.random() * moveSet.length);
   return moveSet[randomNum];
 }
 
-function getPlayerMove(){
-  const playerChoice = window.prompt("Choose either [rock], [paper], or [scissors]").toUpperCase();
-  return playerChoice;
-}
+// function getPlayerMove(){
+//   const playerChoice = window.prompt("Choose either [rock], [paper], or [scissors]").toUpperCase();
+//   return playerChoice;
+// }
 
 function playRound(playerMove, computerMove){
   // Check whether player input legal move
@@ -43,45 +41,56 @@ function playRound(playerMove, computerMove){
   }
 }
 
-function game(){
 
-  // Track number of games won
-  let playerWins = 0;
-  let computerWins = 0;
+// RPS Button Events
+const rockBtn = document.getElementById("rockBtn");
+rockBtn.addEventListener('click', function(){
+  playRound("ROCK", getComputerMove());
+});
 
-  for (let round = 1; round <= 5; round++){
-    console.log("-= ROUND " + round + " =-")
 
-    let roundWinner = playRound(getPlayerMove(), getComputerMove());
 
-    // Increment scoreboard
-    switch(roundWinner){
-      case "tie":
-        round=round-1;
-        break;
-      case "player":
-        playerWins = playerWins + 1;
-        break;
-      case "computer":
-        computerWins = computerWins + 1;
-        break;
-    }
 
-    // Report results
-    console.log("Winner is: " + roundWinner);
-    console.log("");
-    console.log("Player has won " + playerWins + " round(s).");
-    console.log("Computer has won " + computerWins + " round(s).");
-    console.log("");
-  }
 
-  if(playerWins > computerWins){
-    console.log("Player is the winner!");
-  } else {
-    console.log("Computer won...too bad.")
-  }
-}
+// function game(){
 
-game();
+//   // Track number of games won
+//   let playerWins = 0;
+//   let computerWins = 0;
+
+//   for (let round = 1; round <= 5; round++){
+//     console.log("-= ROUND " + round + " =-")
+
+//     let roundWinner = playRound(getPlayerMove(), getComputerMove());
+
+//     // Increment scoreboard
+//     switch(roundWinner){
+//       case "tie":
+//         round=round-1;
+//         break;
+//       case "player":
+//         playerWins = playerWins + 1;
+//         break;
+//       case "computer":
+//         computerWins = computerWins + 1;
+//         break;
+//     }
+
+//     // Report results
+//     console.log("Winner is: " + roundWinner);
+//     console.log("");
+//     console.log("Player has won " + playerWins + " round(s).");
+//     console.log("Computer has won " + computerWins + " round(s).");
+//     console.log("");
+//   }
+
+//   if(playerWins > computerWins){
+//     console.log("Player is the winner!");
+//   } else {
+//     console.log("Computer won...too bad.")
+//   }
+// }
+
+// game();
 
 
